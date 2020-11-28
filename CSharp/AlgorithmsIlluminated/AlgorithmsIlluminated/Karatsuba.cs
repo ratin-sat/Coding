6595e9d6300cb865ee1d420f4f018a9c7c3119b6
Satit Ratinukulkit <ratin_sat@hotmail.com>
2020-11-29 01:42:00 +0900

    AlgorithmsIlluminated: remove simplifying assumption from Karatsuba problem and add more test cases

--- CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/Karatsuba.cs
+++ CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/Karatsuba.cs
@@ -4,7 +4,6 @@
     {
         // Input: two n-digit positive integers x and y
         // Output: the product xy
-        // Assumption: n is a power of 2
         public static string Solve(string x, string y)
         {
             return string.Empty;
