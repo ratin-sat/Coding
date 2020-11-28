e70ceccab90f11882ed7a364e5d26588c9f6d976
Satit Ratinukulkit <ratin_sat@hotmail.com>
2020-11-29 01:21:29 +0900

    AlgorithmsIlluminated: implement test helper function to read each line from text file

---
+++ CSharp/AlgorithmsIlluminated/AlgorithmsIlluminatedTest/TestHelpers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AlgorithmsIlluminatedTest
+{
+    public static class TestHelpers
+    {
+        public static IEnumerable<T> ReadLineFromTextFile<T>(string filepath, Func<string, T> parser)
+            => File.ReadAllLines(filepath).Select(parser);
+    }
+}
