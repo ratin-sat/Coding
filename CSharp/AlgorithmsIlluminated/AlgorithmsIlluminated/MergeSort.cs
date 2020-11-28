332e4c89e0ad2f5ad40db42acadd3cd5bac03dda
Satit Ratinukulkit <ratin_sat@hotmail.com>
2020-11-29 03:52:27 +0900

    AlgorithmsIlluminated: solve MergeSort

--- CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/MergeSort.cs
+++ CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/MergeSort.cs
@@ -1,4 +1,6 @@
-﻿namespace AlgorithmsIlluminated
+﻿using System;
+
+namespace AlgorithmsIlluminated
 {
     public static class MergeSort
     {
@@ -6,7 +8,50 @@
         // Output: array with the same integers, sorted from smallest to largest
         public static int[] Solve(int[] a)
         {
-            return System.Array.Empty<int>();
+            var n = a.Length;
+
+            // base case
+            if (n == 1)
+            {
+                return a;
+            }
+
+            // divide array A into first and second halves
+            var m = n / 2;
+            var h1 = new int[m];
+            var h2 = new int[n - m];
+            Array.Copy(a, 0, h1, 0, m);
+            Array.Copy(a, m, h2, 0, n - m);
+
+            // recursively sort first half of A
+            var c = Solve(h1);
+            // recursively sort second half of A
+            var d = Solve(h2);
+
+            return Merge(c, d);
+        }
+
+        // Input: sorted arrays C and D
+        // Output: sorted array B
+        private static int[] Merge(int[] c, int[] d)
+        {
+            var b = new int[c.Length + d.Length];
+            var bi = 0;
+            var ci = 0;
+            var di = 0;
+            while (bi < b.Length)
+            {
+                if (di >= d.Length || (ci < c.Length && c[ci] < d[di]))
+                {
+                    b[bi++] = c[ci++];
+                }
+                else
+                {
+                    b[bi++] = d[di++];
+                }
+            }
+
+            return b;
         }
     }
 }
