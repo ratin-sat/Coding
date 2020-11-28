7d18c64dff4a7f425897e0be102172f25716bfdf
Satit Ratinukulkit <ratin_sat@hotmail.com>
2020-11-29 02:39:31 +0900

    AlgorithmsIlluminated: update restriction of SecondLargest problem

--- CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/SecondLargest.cs
+++ CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/SecondLargest.cs
@@ -5,6 +5,7 @@
         // Input: unsorted array a of n distinct integers
         // Output: second-largest interger in the array
         // Assumption: n is a power of 2
+        // Restriction: use at most n + log(n) - 2 comparisons
         public static int Solve(int[] a)
         {
             return 0;
