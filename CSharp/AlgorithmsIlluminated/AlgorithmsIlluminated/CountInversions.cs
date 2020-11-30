10266ddbe16fdd3e5c3912387e56bb65ac6779da
Satit Ratinukulkit <ratin_sat@hotmail.com>
2020-11-30 19:20:40 +0900

    AlgorithmsIlluminated: solve CountInversions by brute force

--- CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/CountInversions.cs
+++ CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/CountInversions.cs
@@ -7,7 +7,26 @@
         //   The number of pairs (i, j) of array indices with i < j and A[i] > A[j]
         public static long Solve(int[] a)
         {
-            return 0;
+            return BruteForceCountInv(a);
+        }
+
+        // Input: An array A of distinct integers
+        // Output: The number of inversions of A
+        private static long BruteForceCountInv(int[] a)
+        {
+            var numInv = 0L;
+            for (var i = 0; i < a.Length - 1; i++)
+            {
+                for (var j = i + 1; j < a.Length; j++)
+                {
+                    if (a[i] > a[j])
+                    {
+                        numInv += 1;
+                    }
+                }
+            }
+
+            return numInv;
         }
     }
 }
