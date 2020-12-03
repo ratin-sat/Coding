601c8bd46f7418f874f6cd6f88d9f57828230258
Satit Ratinukulkit <ratin_sat@hotmail.com>
2020-12-04 00:56:13 +0900

    AlgorithmsIlluminated: add problems and test cases for Chapter 5 QuickSort

---
+++ CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/QuickSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AlgorithmsIlluminated
+{
+    public static class QuickSort
+    {
+        // Input: array A of n distinct integers, and a function of choosing pivot element
+        // Postcondition: elements of A are sorted from smallest to largest
+        // Output: number of comparisons
+        public static int Solve(int[] a, Func<int[], int, int, int> choosePivot)
+        {
+            return 0;
+        }
+
+        // Use the first element as the pivot
+        public static int FirstElement(int[] a, int l, int r)
+        {
+            return 0;
+        }
+
+        // Use the last element as the pivot
+        public static int LastElement(int[] a, int l, int r)
+        {
+            return 0;
+        }
+
+        // Use a random element as the pivot
+        public static int RandomElement(int[] a, int l, int r)
+        {
+            return 0;
+        }
+
+        // Consider the first, middle, and final elements,
+        //   then return the median of these elements as the pivot
+        public static int MedianOfThreeElements(int[] a, int l, int r)
+        {
+            return 0;
+        }
+    }
+}
---
+++ CSharp/AlgorithmsIlluminated/AlgorithmsIlluminatedTest/QuickSortTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using AlgorithmsIlluminated;
+using Xunit;
+
+namespace AlgorithmsIlluminatedTest
+{
+    public class QuickSortTest
+    {
+        [Theory]
+        [InlineData(@"resources\Input_Sorting10.txt", @"resources\Output_Sorting10.txt", 25)]
+        [InlineData(@"resources\Input_Sorting100.txt", @"resources\Output_Sorting100.txt", 620)]
+        public void QuickSort_FirstElement(string inputFile, string expectedResultFile, int expectedCount)
+        {
+            var input = TestHelpers.ReadLineFromTextFile(inputFile, int.Parse).ToArray();
+            var postCondition = TestHelpers.ReadLineFromTextFile(expectedResultFile, int.Parse).ToArray();
+            var actual = QuickSort.Solve(input, QuickSort.FirstElement);
+            Assert.Equal(postCondition, input);
+            Assert.Equal(expectedCount, actual);
+        }
+
+        [Theory]
+        [InlineData(@"resources\Input_Sorting10.txt", @"resources\Output_Sorting10.txt", 31)]
+        [InlineData(@"resources\Input_Sorting100.txt", @"resources\Output_Sorting100.txt", 573)]
+        public void QuickSort_LastElement(string inputFile, string expectedResultFile, int expectedCount)
+        {
+            var input = TestHelpers.ReadLineFromTextFile(inputFile, int.Parse).ToArray();
+            var postCondition = TestHelpers.ReadLineFromTextFile(expectedResultFile, int.Parse).ToArray();
+            var actual = QuickSort.Solve(input, QuickSort.LastElement);
+            Assert.Equal(postCondition, input);
+            Assert.Equal(expectedCount, actual);
+        }
+
+        [Theory]
+        [InlineData(@"resources\Input_Sorting10.txt", @"resources\Output_Sorting10.txt", 21)]
+        [InlineData(@"resources\Input_Sorting100.txt", @"resources\Output_Sorting100.txt", 502)]
+        public void QuickSort_MedianOfThreeElements(string inputFile, string expectedResultFile, int expectedCount)
+        {
+            var input = TestHelpers.ReadLineFromTextFile(inputFile, int.Parse).ToArray();
+            var postCondition = TestHelpers.ReadLineFromTextFile(expectedResultFile, int.Parse).ToArray();
+            var actual = QuickSort.Solve(input, QuickSort.MedianOfThreeElements);
+            Assert.Equal(postCondition, input);
+            Assert.Equal(expectedCount, actual);
+        }
+    }
+}
