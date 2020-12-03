262fe3783f47dc2563732da241fa43e1819476a8
Satit Ratinukulkit <ratin_sat@hotmail.com>
2020-12-04 01:32:01 +0900

    AlgorithmsIlluminated: solve HasIdenticalIndexValue

--- CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/HasIdenticalIndexValue.cs
+++ CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/HasIdenticalIndexValue.cs
@@ -6,7 +6,33 @@
         // Output: true if there is an index i such that A[i] = i, otherwise false
         public static bool Solve(int[] a)
         {
-            return false;
+            // find index that A[i] == i in sorted array using binary search
+            return IdenticalIndexBinarySearch(a, 0, a.Length - 1);
+        }
+
+        // Input: sorted (from smallest to largest) array A of n distinct integers,
+        //   left and right endpoints l, r ε {1, 2, ..., n} with l <= r
+        // Output: true if there is an index i such that A[i] = i, otherwise false
+        private static bool IdenticalIndexBinarySearch(int[] a, int l, int r)
+        {
+            if (l <= r)
+            {
+                return l == a[l];
+            }
+
+            var m = (l + r) / 2;
+
+            if (m < a[m])
+            {
+                return IdenticalIndexBinarySearch(a, l, m - 1);
+            }
+
+            if (m > a[m])
+            {
+                return IdenticalIndexBinarySearch(a, m + 1, r);
+            }
+
+            return true;
         }
     }
 }
