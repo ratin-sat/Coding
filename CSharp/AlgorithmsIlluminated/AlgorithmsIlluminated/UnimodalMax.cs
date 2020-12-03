5858a728151fe663be9f45df61ede6b76e05614a
Satit Ratinukulkit <ratin_sat@hotmail.com>
2020-12-04 01:30:08 +0900

    AlgorithmsIlluminated: solve UnimodalMax

--- CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/UnimodalMax.cs
+++ CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/UnimodalMax.cs
@@ -9,7 +9,33 @@
         // Restriction: run in O(log(n)) time
         public static int Solve(int[] u)
         {
-            return 0;
+            // find maximum element in unimodal array using binary search
+            return UnimodalMaxBinarySearch(u, 0, u.Length - 1);
+        }
+
+        // Input: an unimodal array u of n distinct elements,
+        //   left and right endpoints l, r ε {1, 2, ..., n} with l <= r
+        // Output: maximum element of u
+        private static int UnimodalMaxBinarySearch(int[] u, int l, int r)
+        {
+            if (l == r)
+            {
+                return u[l];
+            }
+
+            var m = (l + r) / 2;
+
+            if (u[m] < u[m - 1])
+            {
+                return UnimodalMaxBinarySearch(u, l, m - 1);
+            }
+
+            if (u[m] < u[m + 1])
+            {
+                return UnimodalMaxBinarySearch(u, m + 1, r);
+            }
+
+            return u[m];
         }
     }
 }
