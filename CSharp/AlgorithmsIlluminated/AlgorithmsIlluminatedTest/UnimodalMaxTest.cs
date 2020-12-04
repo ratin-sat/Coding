9ad7256e643003e783a6767d9496c45dea4be8de
Satit Ratinukulkit <ratin_sat@hotmail.com>
2020-12-04 17:44:38 +0900

    AlgorithmsIlluminated: add more test cases for UnimodalMax

--- CSharp/AlgorithmsIlluminated/AlgorithmsIlluminatedTest/UnimodalMaxTest.cs
+++ CSharp/AlgorithmsIlluminated/AlgorithmsIlluminatedTest/UnimodalMaxTest.cs
@@ -12,5 +12,15 @@ namespace AlgorithmsIlluminatedTest
             var actual = UnimodalMax.Solve(input);
             Assert.Equal(8, actual);
         }
+
+        [Theory]
+        [InlineData(new[] { 4, 5, 8, 9, 10, 11, 7, 3, 2, 1 }, 11)]
+        [InlineData(new[] { 11, 9, 8, 7, 5, 4, 3, 2, 1 }, 11)]
+        [InlineData(new[] { 1, 2, 3, 4, 5, 7, 8, 9, 11 }, 11)]
+        public void UnimodalMax_AllPatterns(int[] input, int expected)
+        {
+            var actual = UnimodalMax.Solve(input);
+            Assert.Equal(expected, actual);
+        }
     }
 }
