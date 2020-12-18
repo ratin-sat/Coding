d8d75df3371be77cd1f074ba2e37478e277597c9
Satit Ratinukulkit <ratin_sat@hotmail.com>
2020-12-19 00:05:32 +0900

    AlgorithmsIlluminated: solve Dijkstra

--- CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/Dijkstra.cs
+++ CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/Dijkstra.cs
@@ -1,4 +1,6 @@
-﻿using AlgorithmsIlluminated.DataModel;
+﻿using System.Linq;
+using System.Collections.Generic;
+using AlgorithmsIlluminated.DataModel;
 
 namespace AlgorithmsIlluminated
 {
@@ -9,7 +11,36 @@ namespace AlgorithmsIlluminated
         // Postcondition: for every vertex v, the value len(v) equals the true shortest-path distance dist(s, v)
         public static void Solve(Graph g, Vertex s)
         {
+            SolveWithoutHeap(g, s);
+        }
 
+        // the straightforward implementation of Dijkstra's algorithm without using Heap data structure
+        private static void SolveWithoutHeap(Graph g, Vertex s)
+        {
+            // initialization
+            g.InitializeSearchRecord(s);
+            var len = g.SearchRecord.Distance;
+            len[s] = 0;
+
+            // define Dijkstra score
+            int DScore(Edge e) => len[e.S] + e.Length;
+
+            // set X contains the vertices that the algorithm has already dealt with
+            var x = new HashSet<Vertex> { s };
+            // crosses = list of cross-edge (v, w) with v ε X, w ∉ X
+            var crosses = x.SelectMany(v => g.AdjacencyList[v]).Where(e => !x.Contains(e.V));
+            while (crosses.Count() > 0)
+            {
+                // find an edge minimizing Dijkstra score
+                var vw = crosses.Aggregate((e1, e2) => DScore(e1) < DScore(e2) ? e1 : e2);
+                // assign len-value of w and add w to X
+                var w = vw.V;
+                len[w] = DScore(vw);
+                x.Add(w);
+
+                // update cross-edge list
+                crosses = x.SelectMany(v => g.AdjacencyList[v]).Where(e => !x.Contains(e.V));
+            }
         }
     }
 }
