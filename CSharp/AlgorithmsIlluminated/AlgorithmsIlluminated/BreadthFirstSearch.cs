fde69620bc8f1ce0e737cc5fcb3d23b53a35e3f8
Satit Ratinukulkit <ratin_sat@hotmail.com>
2020-12-18 23:35:39 +0900

    AlgorithmsIlluminated: solve BreadthFirstSearch

--- CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/BreadthFirstSearch.cs
+++ CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/BreadthFirstSearch.cs
@@ -1,4 +1,6 @@
-﻿using AlgorithmsIlluminated.DataModel;
+﻿using System.Linq;
+using System.Collections.Generic;
+using AlgorithmsIlluminated.DataModel;
 
 namespace AlgorithmsIlluminated
 {
@@ -8,7 +10,28 @@ namespace AlgorithmsIlluminated
         // Postcondition: a vertex is reachable from s if and only if it is marked as "explored"
         public static void Solve(Graph g, Vertex s)
         {
+            // mark s as explored, all other vertices as unexplored
+            g.InitializeSearchRecord(s);
+            var explored = g.SearchRecord.Explored;
+            explored[s] = true;
 
+            // Q = a queue data structure, initialized with s
+            var q = new Queue<Vertex>();
+            q.Enqueue(s);
+
+            while (q.Count > 0)
+            {
+                var v = q.Dequeue();
+                foreach (var w in g.AdjacencyList[v].Select(e => e.V))
+                {
+                    if (!explored[w])
+                    {
+                        // mark w as explored
+                        explored[w] = true;
+                        q.Enqueue(w);
+                    }
+                }
+            }
         }
     }
 }
