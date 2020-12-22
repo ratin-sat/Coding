167eecb93157455b03798fe5fe86654d84b9b359
Satit Ratinukulkit <ratin_sat@hotmail.com>
2020-12-23 00:24:56 +0900

    AlgorithmsIlluminated: add SearchTree class (unimplemented)

---
+++ CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/DataStructure/TreeNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AlgorithmsIlluminated.DataStructure
+{
+    public class TreeNode<T> where T : IComparable<T>
+    {
+        public T Content { get; }
+
+        public TreeNode<T> LeftChild { get; set; }
+
+        public TreeNode<T> RightChild { get; set; }
+
+        public TreeNode(T content)
+        {
+            this.Content = content;
+        }
+    }
+}
