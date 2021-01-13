65d5618bb514b74f0c0b48474b92fd4a6ed11584
Satit Ratinukulkit <ratin_sat@hotmail.com>
2021-01-13 21:12:04 +0900

    AlgorithmsIlluminated: solve MedianMaintenance using Heap

--- CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/MedianMaintenance.cs
+++ CSharp/AlgorithmsIlluminated/AlgorithmsIlluminated/MedianMaintenance.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using AlgorithmsIlluminated.DataStructure;
 
 namespace AlgorithmsIlluminated
 {
@@ -9,7 +9,42 @@ namespace AlgorithmsIlluminated
         // Output: sequence of numbers which each element is the median of all elements you've seen thus far
         public static IEnumerable<int> Solve(IEnumerable<int> s)
         {
-            throw new NotImplementedException();
+            return SolveByHeap(s);
+        }
+
+        private static IEnumerable<int> SolveByHeap(IEnumerable<int> s)
+        {
+            // we will negate FindMin, ExtractMin and Insert of h1
+            // to make it behave like a max heap
+            var h1 = new Heap<int>();
+            // standard min heap h2
+            var h2 = new Heap<int>();
+            foreach (var x in s)
+            {
+                var y = h1.Count() > 0 ? -h1.FindMin() : int.MinValue;
+                var z = h2.Count() > 0 ? h2.FindMin() : int.MaxValue;
+
+                if (x < y)
+                {
+                    h1.Insert(-x);
+                }
+                else
+                {
+                    h2.Insert(x);
+                }
+
+                // rebalance h1 and h2
+                if (h1.Count() - h2.Count() >= 2)
+                {
+                    h2.Insert(-h1.ExtractMin());
+                }
+                else if (h2.Count() - h1.Count() >= 2)
+                {
+                    h1.Insert(-h2.ExtractMin());
+                }
+
+                yield return h1.Count() >= h2.Count() ? -h1.FindMin() : h2.FindMin();
+            }
         }
     }
 }
